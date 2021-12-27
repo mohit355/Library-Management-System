42d5b7c2dcc10b5052ad8e0910477552e9a4e75e
mohit355 <mohitraj772002@gmail.com>
2021-12-27 19:57:19 +0530

    added css

--- src/UI/categoryFilter/Filter.js
+++ src/UI/categoryFilter/Filter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import * as actions from "../../state/action/index";
 import { connect } from "react-redux";
 import "./Filter.css";
